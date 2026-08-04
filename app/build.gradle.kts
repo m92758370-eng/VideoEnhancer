bd43f26a64941737c7f87882886274b38f149a7d
m92758370-eng <m92758370@gmail.com>
2026-08-05 03:14:03 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,5 +47,5 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.activity:activity-ktx:1.8.2")
-    implementation("com.arthenica:ffmpeg-kit-full-gpl:6.0-2")
+    implementation("com.antonkarpenko:ffmpeg-kit-full-gpl:2.1.0")
 }
